55acbbd2ed3d2ded6338a1f6c2885e374add07df
quyentt <quyentt1985@gmail.com>
2024-08-06 13:22:23 +0700

    update scroll in bảng điểm

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -177,3 +177,30 @@ $(document).ready(function () {
   $(".masonry-class-reg-1").masonry();
   // $('#themnhom').modal('show');
 });
+// swipe table-----------------------------------------------------------
+const swipeScroll = document.querySelectorAll(".kqht-modal .right");
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+swipeScroll.forEach((slider) => {
+  slider.addEventListener("mousedown", (e) => {
+    isDown = true;
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+  });
+  slider.addEventListener("mouseleave", () => {
+    isDown = false;
+  });
+  slider.addEventListener("mouseup", () => {
+    isDown = false;
+  });
+  slider.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 3; //scroll-fast
+    slider.scrollLeft = scrollLeft - walk;
+  });
+});
